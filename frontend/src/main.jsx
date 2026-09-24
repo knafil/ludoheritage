a3737acb25865ff9d15e43bb5dc1b975aa04dab5
knafil <knafil@yahoo.fr>
2026-09-24 08:25:31 +0000

    fix: i18n import order

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
+import './i18n.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
-import './i18n';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
