8f00408a780b1ffa9108bb183a30579ff012e878
knafil <knafil@yahoo.fr>
2026-09-26 11:31:04 +0000

    feat: passage a react-leaflet avec fond sombre stadia iteration3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -513,11 +513,12 @@ function WorldMap({ games = [], onSelectGame, onOpen }) {
           maxZoom={8}
           style={{ width: "100%", height: "100%", backgroundColor: "#071624" }}
         >
-         <TileLayer
-  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-  url="https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-/>         
- 
+
+        <TileLayer
+  attribution='&copy; <a href="https://www.esri.com/">Esri</a>, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+  url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+  maxZoom={16}
+/> 
          {games.map((game) => {
             const coords = getCoordinates(game);
             const color = getRegionColor(game.region, game.country);
